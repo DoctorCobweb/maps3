ebb7c7ac88196a90ff0df4042f471264732dd48a
Doctor Cobweb <spinninghalf@gmail.com>
2014-03-14 12:41:35 +1100

    refactoring, new routes for gain and lose greens votes

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ var applicationRoot = __dirname,
     CLIENT_APP_DIR = 'dist',
     PORT = process.env.PORT || 5000,
     fs = require('fs'),
-    csv = require('csv');
-
+    csv = require('csv'),
+    MAIN_POLLING_DATA_FILE = '/polling_booth_updated_ANDRE_EDIT_2.csv';
 
 
 // create express app
@@ -41,56 +41,133 @@ app.configure(function () {
 });
 
 
+// helper function: scan through a row, looking for something which satisfies predicate
+// function pred
+function takeWhile(a, pred) {
+  var result = [];
+  var index = undefined;
+
+  for (var i = 0, n = a.length; i < n; i++) {
+    if (pred(a[i], i)) {
+      index = i;
+      break;
+    }
+    //result[i] = i;
+  }
+  //return result;
+  return index;
+}
+
+
+// finds the indexes for columns we need from .csv file
+function getColIndexes (row) {
+      var indexes = {};
+
+  indexes['latitude'] = takeWhile(row, function (n) {
+    return n === 'LATITUDE';
+  }); 
+
+  indexes['longitude'] = takeWhile(row, function (n) {
+    return n === 'LONGITUDE';
+  }); 
+
+  indexes['greens_perc_2010'] = takeWhile(row, function (n) {
+    return n === 'GREENS_PERC_2010';
+  }); 
+      
+  indexes['greens_perc_2006'] = takeWhile(row, function (n) {
+    return n === 'GREENS_PERC_2006';
+  }); 
+
+  console.log(JSON.stringify(indexes));
+  return indexes;
+}
+
+
+function parsePollingFile(cb) {
+  var gps_coords = [],
+      DATA_ROWS_START = undefined,
+      indexes = {};
+
+  csv()
+    .from.path(__dirname + MAIN_POLLING_DATA_FILE, {delimiter: ','})
+    .transform(function (row) {
+      //console.log('row is: ');
+      //console.log(row);
+      return row;
+    })
+    .on('record', function (row, index) {
+      if (DATA_ROWS_START) {
+        gps_coords.push({
+          'latitude': row[indexes.latitude],
+          'longitude': row[indexes.longitude],
+          'greens_perc_2010': row[indexes.greens_perc_2010],
+          'greens_perc_2006': row[indexes.greens_perc_2006],
+          'weight': parseFloat(row[indexes.greens_perc_2010], 10) - parseFloat(row[indexes.greens_perc_2006], 10)
+        });
+      }
+      
+      if (row[0] === 'BOOTH_NAME') {
+        DATA_ROWS_START = index + 1;
+        indexes = getColIndexes(row);
+      }
+
+    })
+    .on('end', function () {
+      console.log('END EVENT fired. finished parsing content');
+      console.log('number of elements: ' + gps_coords.length); //305 booths
+
+      //start the render of content after we've got the gps coordinates
+      cb(gps_coords);
+    })
+    .on('error', function (error) {
+      console.log(error.message);
+    });
+}
+
 
 
 // ------- HTTP ROUTES DEFINITIONS --------------------
 app.get('/', function (req, res) {
     console.log('in GET / route handler');
 
-    var gps_coords = [],
-        LAT_INDEX = 8,  //hard code index for now
-        LONG_INDEX = 9; //hard code index for now
-    
+    parsePollingFile(function (gps_coords) {
+      jade.renderFile('./views/index.jade', {booths: gps_coords}, fn);
 
-    csv()
-      .from.path(__dirname + '/polling_booth_updated_ANDRE_EDIT_2.csv', {delimiter: ','})
-      .transform(function (row) {
-        //console.log('row is: ');
-        //console.log(row);
-        return row;
-      })
-      .on('record', function (row, index) {
-        //console.log(row);
-        //console.log('#'+ index + ' ' + JSON.stringify(row));
-        gps_coords.push({latitude: row[LAT_INDEX], longitude: row[LONG_INDEX]});
-
-      })
-      .on('end', function () {
-        console.log('END EVENT fired. finished parsing content');
-        //console.dir(gps_coords.slice(1));
-        console.log('number of elements: ' + gps_coords.slice(1).length); //305 booths
-
-        //start the render of content after we've got the gps coordinates
-        render();
-      })
-      .on('error', function (error) {
-        console.log(error.message);
-      });
-
-
-    function render () {
-        jade.renderFile('./views/index.jade', {booths: gps_coords.slice(1)}, cb);
-
-        function cb (err, html) {
-            if (err) throw err;
-            return res.send(html);
-        }
-    }
+      function fn (err, html) {
+          if (err) throw err;
+          return res.send(html);
+      }
+    });
 });
 
 
+app.get('/greens/gain', function (req, res) {
+    console.log('in GET /greens/gain route handler');
+
+    parsePollingFile(function (gps_coords) {
+      jade.renderFile('./views/greensGain.jade', {booths: gps_coords}, fn);
+
+      function fn (err, html) {
+          if (err) throw err;
+          return res.send(html);
+      }
+    });
+});
 
 
+app.get('/greens/lose', function (req, res) {
+    console.log('in GET /greens/lose route handler');
+
+    parsePollingFile(function (gps_coords) {
+      jade.renderFile('./views/greensLose.jade', {booths: gps_coords}, fn);
+
+      function fn (err, html) {
+          if (err) throw err;
+          return res.send(html);
+      }
+    });
+});
 
 // ------- START THE SERVER --------------------------------
 
